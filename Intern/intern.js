a1345a0744c06ec153c1d77a9aa200378c77026b
Hasibul Pailan <hasibulpailan8@gmail.com>
2024-02-05 18:34:31 +0530

    adding all initial changes

---
+++ Intern/intern.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    const posts = [
+      {
+        type: 'article',
+        title: 'What if famous brands had regular fonts? Meet RegulaBrands!',
+        author: 'Sarthak Kamra',
+        views: '1.4k',
+        content: 'I\'ve worked in UX for the better part of a decade. Fusce dapibus, tellus ac cursus commodo...'
+      },
+      {
+        type: 'education',
+        title: 'Tax Benefits for Investment under National Pension Scheme launched by Government',
+        author: 'Sarah West',
+        views: '4.8k',
+        content: 'I\'ve worked in UX for the better part of a decade. Fusce dapibus, tellus ac cursus commodo...'
+      },
+      {
+        type: 'meetup',
+        title: 'Finance & Investment Elite Social Mixer @Lujiazui',
+        author: 'Ronal Jones',
+        views: '800',
+        date: 'Fri, 12 Oct, 2018',
+        location: 'Lujiazui'
+      },
+      {
+        type: 'job',
+        title: 'Software Developer - II',
+        company: 'Innovaccer Analytics',
+        location: 'Ahmedabad, India',
+        url: 'https://www.timesjobs.com/...',
+        source: 'Timesjobs'
+      })
+    ]
